f809e6bcd71f5504edef65b4d419365cc190a9ad
ManelKhayat <manelkhayat2020@gmail.com>
2020-11-19 14:40:43 +0100

    commit

--- app.js
+++ app.js
@@ -55,8 +55,10 @@ Btns_ajout.forEach( btn => {
 function inserer_DOM(produitObj){
     panier.insertAdjacentHTML('beforeend', `
     <div class="article_panier container">
-        <img class="img_article_panier" src="${produitObj.img}" alt="${produitObj.nom}"><br><br>
-        <h3 class="nom_article_panier">${produitObj.nom}</h3>
+        <div>
+            <img class="img_article_panier" src="${produitObj.img}" alt="${produitObj.nom}">
+            <h3 class="nom_article_panier">${produitObj.nom}</h3>
+        </div>
         <h3 class="prix_article_panier ">${produitObj.prix}</h3>
         <button class="btn btn--primary btn--small btn-space${(produitObj.quantité === 1 ? ' btn--danger':'')}" data-action="diminuer" style="text-align:center;border-radius:10px;width:94%;"><i class="fas fa-minus"></i></button>
         <h3 class="quantité_article">${produitObj.quantité}</h3>
